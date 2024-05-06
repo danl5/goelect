a76bc8d3cb0268751b87785a6832a4807e972560
danl5 <danielip@yeah.net>
2024-05-06 22:31:03 +0800

    refine rpc handler

---
+++ internal/consensus/consensus_rpc.go
@@ -0,0 +1,110 @@
+package consensus
+
+import (
+	"github.com/danl5/goelect/internal/common"
+	"github.com/danl5/goelect/internal/config"
+	"github.com/danl5/goelect/internal/log"
+	"github.com/danl5/goelect/internal/model"
+)
+
+func NewConsensusRpcHandler(cfg *config.Config, logger log.Logger, node model.ElectNode) (*RpcHandler, error) {
+	c, err := NewConsensus(cfg, logger, node)
+	if err != nil {
+		logger.Error("failed to create a Consensus instance", "err", err.Error())
+		return nil, err
+	}
+	return &RpcHandler{c}, nil
+}
+
+type RpcHandler struct {
+	*Consensus
+}
+
+// Run starts the consensus
+// Returns a channel of state transitions and an error
+func (c *RpcHandler) Run() (<-chan model.StateTransition, error) {
+	return c.Consensus.Run()
+}
+
+// HeartBeat handles heartbeat request from peer node
+func (c *RpcHandler) HeartBeat(args *model.HeartBeatRequest, reply *model.HeartBeatResponse) error {
+	c.logger.Debug("receive heartbeat", "from", args.NodeId)
+
+	if c.term > args.Term {
+		// term in the request is behind this node
+		model.HBResponse(reply, false, common.HeartbeatExpired.String())
+		return nil
+	}
+
+	// update term of this node
+	c.setTerm(args.Term)
+
+	switch c.node.State {
+	case model.NodeStateLeader:
+		// leave leader state
+		c.sendEvent(model.EventLeaveLeader)
+	case model.NodeStateFollower:
+		// send heartbeat to handler
+		c.followerChan <- struct{}{}
+	case model.NodeStateCandidate:
+		// receive a new leader
+		c.sendEvent(model.EventNewLeader)
+	case model.NodeStateDown:
+	}
+
+	model.HBResponse(reply, true, common.HeartbeatOk.String())
+	return nil
+}
+
+// RequestVote handle vote request from peer node
+func (c *RpcHandler) RequestVote(args *model.RequestVoteRequest, reply *model.RequestVoteResponse) error {
+	c.logger.Info("receive vote request", "from", args.NodeAddr, "term", args.Term, "current term", c.term)
+	// return when novote is true
+	if c.node.NoVote {
+		model.VoteResponse(reply, c.node.Node, false, common.VoteNoVoteNode.String())
+		return nil
+	}
+
+	switch c.node.State {
+	case model.NodeStateLeader:
+		if args.Term <= c.term {
+			model.VoteResponse(reply, c.node.Node, false, common.VoteLeaderExist.String())
+			return nil
+		}
+		// term in the request is newer, leaves leader state
+		c.sendEvent(model.EventLeaveLeader)
+	case model.NodeStateFollower:
+		if args.Term < c.term {
+			model.VoteResponse(reply, c.node.Node, false, common.VoteTermExpired.String())
+			return nil
+		}
+	case model.NodeStateCandidate:
+		if args.Term <= c.term {
+			model.VoteResponse(reply, c.node.Node, false, common.VoteHaveVoted.String())
+			return nil
+		}
+		// term in the request is newer, vote and switch to follower state
+		c.sendEvent(model.EventNewTerm)
+	case model.NodeStateDown:
+	}
+
+	// update term cache
+	c.setTerm(args.Term)
+	c.vote(args.Term, args.NodeId)
+
+	c.logger.Info("vote for", "node", args.NodeId, "term", args.Term)
+	model.VoteResponse(reply, c.node.Node, true, common.VoteOk.String())
+	return nil
+}
+
+// Ping handles ping request from peer node
+func (c *RpcHandler) Ping(args struct{}, reply *string) error {
+	*reply = "pong"
+	return nil
+}
+
+// State return current node state
+func (c *RpcHandler) State(args struct{}, reply *model.ElectNode) error {
+	*reply = c.CurrentState()
+	return nil
+}
